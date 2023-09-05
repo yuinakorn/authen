08764ae7fbc8653ee0168715d217278d058d7cbe
yuinakorn <yuinakorn@hotmail.com>
2023-09-05 10:19:08 +0700

    gunicorn

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World"}
+
+
 @app.get("/callback/")
 async def callback():
     return {"message": "use for POST method"}
