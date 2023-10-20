9cbd5e63eb3ec5a3464ddc0f923251a0fedae596
yuinakorn <yuinakorn@hotmail.com>
2023-10-20 20:23:07 +0700

    step3 test5

--- main.py
+++ main.py
@@ -45,13 +45,11 @@ async def callback(code: str = None, state: str = None):
 
             print(response2.text)
 
-            return {"active": response2.json()["active"],"detail": response.json()}
-
-            # if response2.json()["active"] is True:
-            #     return response.json()
-            # else:
-            #     raise HTTPException(status_code=400, detail="Invalid input. Code and state are required.")
-            #
+            if response2.json()["active"] is True:
+                return {"active": response2.json()["active"], "detail": response.json()}
+                # return response.json()
+            else:
+                raise HTTPException(status_code=400, detail="Invalid input. Code and state are required.")
 
             # return response.json()
 
