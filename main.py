b1774fab2f61f7d0ef6607c29bcd02d16b09de21
yuinakorn <yuinakorn@hotmail.com>
2023-11-04 14:36:37 +0700

    cors

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from dotenv import dotenv_values
 from fastapi import FastAPI, HTTPException
 from routers import auth_router
 
+# allow CORS origin
+from fastapi.middleware.cors import CORSMiddleware
+
+
 config_env = dotenv_values(".env")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router)
 app.include_router(auth_router.router2)
