54e65b480c0d7fba01f3d4da13652905fb502f97
yuinakorn <yuinakorn@hotmail.com>
2024-04-25 00:42:43 +0700

    client ip x-forword

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -240,12 +240,13 @@ def get_public_ip():
 
 def get_client(request):
     client_ip = request.client.host
-    public_ip = get_public_ip()
+    public_ip = request.headers.get('x-forwarded-for')
+    ip_address = public_ip + " " + client_ip
     user_agent_string = request.headers.get('user-agent')
     user_agent = parse(user_agent_string)
     browser = user_agent.browser.family if user_agent.browser else "Unknown"
     operating_system = user_agent.os.family if user_agent.os else "Unknown"
-    ip_address = public_ip + " " + client_ip
+
     return {"client_ip": ip_address, "browser": browser, "os": operating_system, "user_agent": user_agent}
 
 
